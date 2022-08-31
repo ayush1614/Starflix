87920ebdc81dae320d3dd191e4e791784b0d01b2
Ayush Katiyar <katiyarayush2000@gmail.com>
2022-08-31 13:49:02 +0530

    header completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,36 +7,38 @@ function Header() {
         <Nav>
             <Logo src='/images/logo.svg' />
             <NavMenu >
-                <a href='/'>
+                <a>
                     <img src="/images/icons/home-icon.svg" alt="home-icon" />
                     <span>HOME</span>
                 </a>
 
-                <a href="/">
+                <a>
                     <img src="/images/icons/search-icon.svg" alt="search-icon" />
                     <span>SEARCH</span>
                 </a>
 
-                <a href="/">
+                <a>
                     <img src="/images/icons/watchlist-icon.svg" alt="watchlist-icon" />
                     <span>WATCHLIST</span>
                 </a>
 
-                <a href="/">
+                <a>
                     <img src="/images/icons/original-icon.svg" alt="original-icon" />
                     <span>ORIGINALS</span>
                 </a>
-                
-                <a href="/">
+
+                <a>
                     <img src="/images/icons/movie-icon.svg" alt="movie-icon" />
                     <span>MOVIES</span>
                 </a>
-                
-                <a href="/">
+
+                <a>
                     <img src="/images/icons/series-icon.svg" alt="series-icon" />
                     <span>SERIES</span>
                 </a>
             </NavMenu>
+
+            <UserImage src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-squares-01/3/30-512.png" alt='user-icon' />
         </Nav>
     )
 }
@@ -58,21 +60,52 @@ const Logo = styled.img`
 
 const NavMenu = styled.div`
     display :flex ;
+    flex: 1;
+    margin: 20px ;
+    align-items: center ;
 
     a{
         display: flex ;
-        text-decoration: none ; 
-        color:white;
         align-items: center ;
         padding: 0 12px ;
-
+        cursor:pointer ;
         img{
+            margin: 8px ;
             height: 20px ;
         }
 
         span{
+            font-family: "Roboto","HelveticaNeue-Light",sans-serif;
             font-size: 13px ;
-            letter-spacing: 1.42px
+            letter-spacing: 1.42px;
+            position: relative ;
+
+            &:after{
+                content: "" ; 
+                height: 2px ; 
+                background: white ;
+                position: absolute ;
+                left:0 ;
+                right: 0 ;
+                bottom: -6px ;
+                opacity: 0 ;
+                transform: scaleX(0) ; 
+            }
+        }
+        
+        &:hover{
+            span:after{
+                transition: transform 0.25s ease-out;
+                transform-origin : center ; 
+                transform: scaleX(1) ; 
+                opacity: 1 ; 
+            }
         }
     }
+`
+const UserImage = styled.img`
+    width: 48px ; 
+    height: 48px ;
+    border-radius: 50% ; 
+    cursor: pointer ; 
 `
